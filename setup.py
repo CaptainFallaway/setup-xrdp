04dddc31eb548c94e29b2879bc0aba5cbb1bd802
CaptainFallaway <69320425+CaptainFallaway@users.noreply.github.com>
2020-11-03 21:28:03 +0100

    Updated setup.py
    Made a goof by not putting a ":" in the end of the "IF" argument

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import *
 from time import sleep
 
 # Main
-print("If you run setup.py then the directory will change name from setup-xrdp to setup beacause the easier access to the directory, it will also delete setup.py for cleanliness")
+print("If you run setup.py then the directory will change name from setup-xrdp to setup beacause the easier access to the directory, it will also delete setup.py for cleanliness.")
 inputed = input("Are you sure you want to do this? (y/n): ")
 if inputed == "y" or inputed == "Y":
 	system("cd ..")
